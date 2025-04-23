31c09830c9d890c96834b6522afb7d44c2ecc12c
Naing Linn Aung <naing@evantek.com>
2025-04-23 08:57:16 +0800

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let items = ["One", "Two", "Three", "Four", "Five"];
+  const handleSelectedItem = (item: string) => {
+    return <Alert>Hello World</Alert>;
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+      <ListGroup
+        items={items}
+        heading="Numbers"
+        onSelectedItem={handleSelectedItem}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
