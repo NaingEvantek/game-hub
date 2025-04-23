ec0ecce7ae4ce0cfb0687c508f9d7ee8ca5a3c0a
Naing Linn Aung <naing@evantek.com>
2025-04-23 09:29:31 +0800

    Exercise - Showing an Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let items = ["One", "Two", "Three", "Four", "Five"];
   const handleSelectedItem = (item: string) => {
     console.log(item);
   };
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleCloseAlert = () => {
+    setAlertVisibility(!alertVisible);
+  };
   return (
     <div>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      {alertVisible && <Alert onClose={handleCloseAlert}>My alert</Alert>}
+      <Button color="primary" onClick={handleCloseAlert}>
         Click with Children
       </Button>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      {/* 
       <ListGroup
         items={items}
         heading="Numbers"
         onSelectedItem={handleSelectedItem}
-      />
+      /> */}
     </div>
   );
 }
